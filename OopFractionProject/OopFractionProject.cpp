c62f3a52ab1772cecf6b025e92bc25c343d6bde9
maxefimov <efimov.top.academy@gmail.com>
2025-02-11 10:29:59 +0300

    11-02-25

--- OopFractionProject/OopFractionProject.cpp
+++ OopFractionProject/OopFractionProject.cpp
@@ -119,6 +119,75 @@ public:
         result.Shorten();
         return result;
     }
+
+    Fraction operator+(Fraction f)
+    {
+        Fraction result;
+        result.numerator = numerator * f.denominator + f.numerator * denominator;
+        result.denominator = denominator * f.denominator;
+        result.Shorten();
+        return result;
+    }
+
+    Fraction operator+(int number)
+    {
+        Fraction f;
+        f.SetNumerator(number * denominator);
+        f.SetDenominator(denominator);
+        return this->Plus(f);
+    }
+
+    friend Fraction operator+(int number, Fraction f)
+    {
+        return f + number;
+    }
+
+    friend bool operator<(Fraction f1, Fraction f2)
+    {
+        return f1.numerator * f2.denominator < f2.numerator * f1.denominator;
+    }
+
+    friend bool operator>=(Fraction f1, Fraction f2)
+    {
+        return !(f1 < f2);
+    }
+
+    friend bool operator==(Fraction f1, Fraction f2)
+    {
+        return f1.numerator * f2.denominator == f2.numerator * f1.denominator;
+    }
+
+    friend bool operator!=(Fraction f1, Fraction f2)
+    {
+        return !(f1 == f2);
+    }
+
+    Fraction operator-() 
+    {
+        Fraction result{ *this };
+        result.numerator = -result.numerator;
+        return result;
+    }
+
+    Fraction operator++()
+    {
+        *this = *this + 1;
+        return *this;
+    }
+
+    Fraction operator++(int)
+    {
+        Fraction result{ *this };
+        ++(*this);
+        return result;
+    }
+
+    friend std::ostream& operator<<(std::ostream& out, const Fraction& f)
+    {
+        out << "[" << f.numerator << "/"
+            << f.denominator << "]";
+        return out;
+    }
 };
 
 int main()
@@ -127,7 +196,10 @@ int main()
     f1.Cout();
     std::cout << "\n";
 
-    Fraction f2(2, 7);
+    Fraction f2 = 2 + f1++;
+    std::cout << f1 << " " << f2 << "\n";
+
+    /*Fraction f2(2, 7);
     f2.Cout();
     std::cout << "\n";
 
@@ -136,7 +208,7 @@ int main()
     f3.SetNumerator(2);
     f3.SetDenominator(5);
     f3.Cout();
-    std::cout << "\n";
+    std::cout << "\n";*/
 
     /*Fraction f4 = f1.Plus(f2)
                     .Minus(f3)
